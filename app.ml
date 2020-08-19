acfd437b6a52655f5e7d2669f86ef29bbd453fa1
Josef Erben <josef@erben.io>
2020-08-19 18:24:09 +0200

    Add .ocamlformat and format existing code

--- app.ml
+++ app.ml
@@ -1,7 +1,5 @@
-let services: (module Sihl.Core.Container.SERVICE) list =
-  [
-    (module Service.WebServer);
-  ]
+let services : (module Sihl.Core.Container.SERVICE) list =
+  [ (module Service.WebServer) ]
 
 let hello_page =
   Sihl.Web.Route.get "/hello/" (fun _ ->
@@ -11,15 +9,8 @@ let hello_api =
   Sihl.Web.Route.get "/hello/" (fun _ ->
       Sihl.Web.Res.(json |> set_body {|{"msg":"Hello!"}|}) |> Lwt.return)
 
-let routes =
-  [ ("/page", [ hello_page ], []); ("/api", [ hello_api ], []) ]
+let routes = [ ("/page", [ hello_page ], []); ("/api", [ hello_api ], []) ]
 
 module App = Sihl.App.Make (Service)
 
-let _ =
-  App.(
-    empty
-    |> with_services services
-    |> with_routes routes
-    |> run
-  )
+let _ = App.(empty |> with_services services |> with_routes routes |> run)
